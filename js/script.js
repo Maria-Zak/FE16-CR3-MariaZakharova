e542a0e31ddc70be22804af4afef0652258b9f10
Maria Zakharova <zakharovamary@gmail.com>
2022-07-04 22:50:53 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
-var priceArkona = 50;
-var priceStigmata = 40;
-var priceZmey = 20;
-var priceLouna = 50;
-var priceAmatory = 40;
-var priceWildways = 20;
-var priceWoodscream = 50;
-var priceSlaughter = 20;
-var priceTeufelstanz = 20;
+var priceGroup1 = 50;
+var priceGroup2 = 40;
+var priceGroup3 = 20;
+var priceGroup4 = 40;
+var priceGroup5 = 30;
+var priceGroup6 = 20;
+var priceGroup7 = 50;
+var priceGroup8 = 20;
+var priceGroup9 = 20;
 
-var fest_day1 = 'Arkona, Louna and Woodscream'
-var fest_day2 = 'Stigmata, Amatory and Slaughter to Prevail'
-var fest_day3 = 'Zmey Gorynich, Wildways and Teufelstanz'
+var fest_day1 = 'Group 1, Group 2 and Group 3';
+var fest_day2 = 'Group 4, Group 5 and Group 6';
+var fest_day3 = 'Group 7, Group 8 and Group 9';
+
+var day1_price = priceGroup1 + priceGroup2 + priceGroup3;
+var day2_price = priceGroup4 + priceGroup5 + priceGroup6;
+var day3_price = priceGroup7 + priceGroup8 + priceGroup9;
+
 
 console.log('On the first day of the festival, I was lucky to watch bands ' + fest_day1 + ".")
-console.log('Then I watched the bands ' + fest_day2 + ', on the second day and it cost me ' + (priceStigmata + priceAmatory + priceSlaughter) + '€ in total that day.')
-console.log('I was a bit tired on the third day but it was so much worth it to watch the bands ' + fest_day3 + ' and I spent ' + (priceZmey + priceWildways + priceTeufelstanz) + '€ for the three days of the festival.')
+console.log('Then I watched the bands ' + fest_day2 + ', on the second day and it cost me ' + (priceGroup4 + priceGroup5 + priceGroup6) + '€ in total that day.')
+console.log('I was a bit tired on the third day but it was so much worth it to watch the bands ' + fest_day3 + ' and I spent ' + (priceGroup7 + priceGroup8 + priceGroup9) + '€ for the three days of the festival.')
 
-var totalPrice = priceAmatory + priceArkona + priceLouna + priceSlaughter + priceStigmata + priceTeufelstanz + priceWildways + priceWoodscream + priceZmey
+console.log('Then I watched the bands ' + fest_day2 + ', on the second day and it cost me ' + day2_price + '€ in total that day.')
+console.log('I was a bit tired on the third day but it was so much worth it to watch the bands ' + fest_day3 + ' and I spent ' + day3_price + '€ for the three days of the festival.')
+
+
+var totalPrice = priceGroup1 + priceGroup2 + priceGroup3 + priceGroup4 + priceGroup5 + priceGroup6 + priceGroup7 + priceGroup8 + priceGroup9
+var totalPriceAlt = day1_price + day2_price + day3_price
 console.log('If I had booked the concert with enough time in advance, it would have cost me a total of ' + (totalPrice - totalPrice * 0.3) + '€ and I would have saved ' + (totalPrice * 0.3) + '€.')
